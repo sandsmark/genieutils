e3d5efc75c226571bb0d4e78d16a816e24526c85
Mikko <mikko.partonen@metropolia.fi>
2016-02-29 16:30:51 +0200

    Easy to build using the MinGW bundled with Qt.
    No need to mess with Qt Creator at all.
    Just use the provided mingw32-make to ensure that the libraries are
    compatible.
    Also need to build Boost Iostreams with it.

---
+++ qBuildDebug.bat
@@ -0,0 +1,9 @@
+CD ../genieutils-build/debug/
+SET zlibIF="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\include"
+SET zlibLL="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\lib\libz.a"
+SET iconvIF="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\include"
+SET iconvLL="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\lib\libiconv.a"
+cmake -j4 -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DGUTILS_TOOLS=0 -DGUTILS_TEST=0 -DZLIB_INCLUDE_DIR="%zlibIF%" -DZLIB_LIBRARY="%zlibLL%" -DICONV_INCLUDE_DIR="%iconvIF%" -DICONV_LIBRARIES="%iconvLL%" ../../genieutils/
+mingw32-make
+CD ../../genieutils/
+PAUSE
---
+++ qBuildRelease.bat
@@ -0,0 +1,10 @@
+CD ../genieutils-build/release/
+SET zlibIF="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\include"
+SET zlibLL="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\lib\libz.a"
+SET iconvIF="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\include"
+SET iconvLL="H:\Qt\Tools\mingw492_32\i686-w64-mingw32\lib\libiconv.a"
+cmake -j4 -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DGUTILS_TOOLS=0 -DGUTILS_TEST=0 -DZLIB_INCLUDE_DIR="%zlibIF%" -DZLIB_LIBRARY="%zlibLL%" -DICONV_INCLUDE_DIR="%iconvIF%" -DICONV_LIBRARIES="%iconvLL%" ../../genieutils/
+mingw32-make
+strip libgenieutils.a
+CD ../../genieutils/
+PAUSE
