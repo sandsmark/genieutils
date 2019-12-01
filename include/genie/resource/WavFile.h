86f7acffb0f140bde0d8cf8b1a902e80067c3863
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-12-01 13:58:58 +0100

    wip

--- include/genie/resource/WavFile.h
+++ include/genie/resource/WavFile.h
@@ -11,41 +11,71 @@ namespace genie {
 
 class WavFile : public IFile
 {
+
 public:
     WavFile();
 
-    struct WavHeader {
-        // RIFF header
-        uint32_t ChunkID{};
-        uint32_t ChunkSize{};
-        uint32_t Format{};
+    struct RiffHeader {
+        static constexpr std::array<uint8_t, 4> expectedId = {'R', 'I', 'F', 'F'};
+        std::array<uint8_t, 4> id = {'R', 'I', 'F', 'F'};
 
-        // fmt subchunk
-        uint32_t Subchunk1ID{};
-        uint32_t Subchunk1Size{};
+        uint32_t dataSize = 0; // the file size minus riff and wave header
+    };
 
-        enum AudioFormats {
+    struct WaveHeader {
+        static constexpr std::array<uint8_t, 4> expectedId = {'W', 'A', 'V', 'E'};
+        std::array<uint8_t, 4> id = {'W', 'A', 'V', 'E'};
+    };
+
+    struct ChunkHeader {
+        enum ChunkTypes : uint32_t {
+            Format = 0x20746d66u, // 'fmt '
+            Data = 0x61746164u, // 'data'
+
+            // the rest are 'cue ', 'LIST', 'smpl and 'vorb',
+            // but I can't be assed to convert to hex mentally
+        } type{};
+
+        uint32_t size{};
+    };
+
+    struct WavFormatChunk : public ChunkHeader {
+        enum AudioFormats : uint16_t {
             PCM = 0x1,
             ADPCM = 0x2,
             IEEEFloat = 0x3,
             ALaw = 0x6,
             MULaw = 0x7,
-            DVIADPCM = 0x11
-        };
-        uint16_t AudioFormat{};
+            DVIADPCM = 0x11,
+            AAC = 0xff,
+            Wwise = 0xffffu,
+        } codec{};
 
-        uint16_t NumChannels{};
-        uint32_t SampleRate{};
-        uint32_t ByteRate{};
-        uint16_t BlockAlign{};
-        uint16_t BitsPerSample{};
-
-        // data subchunk
-        uint32_t Subchunk2ID{};
-        uint32_t Subchunk2Size{};
+        uint16_t numChannels{};
+        uint32_t sampleRate{};
+        uint32_t byteRate{};
+        uint16_t blockAlign{};
+        uint16_t bitsPerSample{};
     };
 
-    const WavHeader &header() { return m_header; }
+    struct WavHeader {
+        RiffHeader riffHeader;
+        WaveHeader waveHeader;
+
+        // We hardcode the chunk layout here (because that's how all files seem to be in practice)
+        WavFormatChunk format;
+    };
+
+    struct WwiseData {
+        // ??
+        uint16_t formatLength2;
+        uint16_t unknown1{};
+        uint16_t subType{};
+        char whoknows[0x18];
+//        uint16_t unknown2[0x10];
+    } __attribute__((packed));
+
+    const WavFormatChunk &format() { return m_header.format; }
     const uint8_t *audioData() { return m_data.data(); }
 
 
@@ -58,7 +88,12 @@ private:
 
     uint32_t m_size = 0;
     uint32_t m_type = 0;
+
     WavHeader m_header;
+    WwiseData m_extraData;
+
+    ChunkHeader m_dataHeader;
+
     std::vector<uint8_t> m_data;
 };
 
--- src/resource/WavFile.cpp
+++ src/resource/WavFile.cpp
@@ -14,13 +14,50 @@ WavFile::WavFile()
 
 void WavFile::serializeObject()
 {
+    static_assert(sizeof(RiffHeader) == 8);
+    static_assert(sizeof(WaveHeader) == 4);
+    static_assert(sizeof(ChunkHeader) == 8);
+    static_assert(sizeof(WavFormatChunk) == 24);
+//    static_assert(sizeof(WwiseData) == 48);
+
     serialize<WavHeader>(m_header);
-    if (m_header.Subchunk2Size >= std::numeric_limits<int>::max()) {
+    if (m_header.riffHeader.id != RiffHeader::expectedId) {
+        log.warn("Invalid riff header %", m_header.riffHeader.id.data());
+        return;
+    }
+
+    if (m_header.waveHeader.id != WaveHeader::expectedId) {
+        log.warn("Invalid wave header %", m_header.waveHeader.id.data());
+        return;
+    }
+
+    if (m_header.format.codec == WavFormatChunk::Wwise) {
+        serialize<WwiseData>(m_extraData);
+        log.debug("align %", m_header.format.blockAlign);
+        log.debug("bps %", m_header.format.bitsPerSample);
+        log.debug("extra %", m_extraData.formatLength2);
+        log.debug("unk %", m_extraData.unknown1);
+        log.debug("subtype %", m_extraData.subType);
+
+        if (m_header.format.size != sizeof(WavFormatChunk) + sizeof(WwiseData)) {
+//            for (size_t i=0; i<0x18; i++) {
+//                std::cout << std::hex << (uint(m_extraData.whoknows[i]) & 0xff) << std::endl;
+
+//            }
+            log.warn("Invalid format chunk, size is % but expected %", m_header.format.size, (sizeof(WavFormatChunk) + sizeof(WwiseData)));
+            return;
+        }
+    }
+
+    serialize<ChunkHeader>(m_dataHeader);
+
+    if (m_dataHeader.size >= std::numeric_limits<int>::max()) {
         log.warn("I seriously doubt there's a wav file this big, remove this check if that's the case");
         return;
     }
 
-    serialize<uint8_t>(m_data, m_header.Subchunk2Size - sizeof(WavHeader));
+    serialize<uint8_t>(m_data, m_dataHeader.size - sizeof(ChunkHeader));
+    log.warn("test");
 }
 
 } // namespace genie
