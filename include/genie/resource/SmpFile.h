a397d932d9caf943e0c35191416408f2b6c8fefa
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-12-09 22:32:13 +0100

    switch to tapsa's smpfile implementation until I can be arsed to fix AGE properly

--- include/genie/resource/SmpFile.h
+++ include/genie/resource/SmpFile.h
@@ -1,83 +1,119 @@
-#pragma once
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2018  Mikko "Tapsa" P
 
-// Documentation copyright Christoph Heine, because I can't be assed to rewrite what he wrote
-// From openage/doc/media/smx-files.md
-// From openage/doc/media/smp-files.md
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GENIE_SMPFILE_H
+#define GENIE_SMPFILE_H
+
+#include <istream>
+#include <vector>
 
 #include "genie/file/IFile.h"
 #include "genie/util/Logger.h"
-
+#include "PalFile.h"
 #include "SmpFrame.h"
 
-#include <array>
+namespace genie
+{
 
-namespace genie {
-
-/**
- * @brief SMP is the sprite storage format of Age of Empires: Definitive Edition
- */
-
+//------------------------------------------------------------------------------
+/// A smp file stores one or several images encoded using simple commands.
+/// The image is stored as 8 bits per pixel, that means only the index of a
+/// color in a palette is saved.
+//
 class SmpFile : public IFile
 {
-    static constexpr std::array<uint8_t, 4> smpHeader = {'S', 'M', 'P', '$' };
 
 public:
-    const SmpFrame &frame(size_t frameNum = 0) {
-        if (frameNum >= m_frames.size()) {
-            std::cerr << "invalid framenum" << frameNum << std::endl;
-            return SmpFrame::null;
-        }
-        return m_frames[frameNum];
-    }
+  //----------------------------------------------------------------------------
+  /// Constructor
+  //
+  SmpFile();
 
-    // ISerializable interface
-protected:
-    void serializeObject() override;
+  //----------------------------------------------------------------------------
+  /// Destructor
+  //
+  virtual ~SmpFile();
+
+  //----------------------------------------------------------------------------
+  /// Frees all content of a smp file.
+  //
+  void unload(void);
+
+  //----------------------------------------------------------------------------
+  /// Check whether the files content is loaded or not.
+  //
+  bool isLoaded(void) const;
+
+  //----------------------------------------------------------------------------
+  /// Return number of frames stored in the file. Available after load.
+  ///
+  /// @return number of frames
+  //
+  uint32_t getFrameCount(void);
+  void setFrameCount(uint32_t);
+
+  //----------------------------------------------------------------------------
+  /// Returns the smp frame at given frame index.
+  ///
+  /// @param frame frame index
+  /// @return SmpFrame
+  //
+  SmpFramePtr getFrame(uint32_t frame=0);
+  void setFrame(uint32_t, SmpFramePtr);
+
+  std::string signature;
+  std::string comment;
+  uint32_t version;
+  uint32_t num_animations;
+  uint32_t num_frames_per_animation;
+  uint32_t checksum;
+  uint32_t size;
+  uint32_t resource_id;
 
 private:
-    static Logger &log;
+  static Logger &log;
 
-    /// File descriptor
-    /// Always SMPX
-    std::array<uint8_t, 4> m_header = {'S', 'M', 'P', '$' };
+  bool loaded_ = false;
 
-    /// possibly version
-    /// Ex: 256, 0x00000100 (same value for almost all units)
-    int32_t m_version2 = 0;
+  uint32_t num_frames_ = 0;
 
-    /// Number of frames
-    /// Ex: 721, 0x000002D1
-    int32_t m_numFrames = 0;
+  std::vector<uint32_t> frame_offsets_;
+  typedef std::vector<SmpFramePtr> FrameVector;
+  FrameVector frames_;
 
-    /// File size SMX (this file)
-    /// Ex: 2706603, 0x000294CAB (size without header)
-    uint32_t m_size = 0;
+  // Used to calculate offsets when saving the SMP.
+  uint32_t smp_offset_;
 
-    /// ??
-    /// 1, 0x0000001 (almost always 0x00000001)
-    int32_t m_unknown;
+  //----------------------------------------------------------------------------
+  virtual void serializeObject(void);
 
-    /// Number of frames
-    /// 721, 0x000002D1 (0x00000001 for version 0x0B)
-    int32_t m_numFrames2;
+  //----------------------------------------------------------------------------
+  /// Loads the file and its frames.
+  //
+  void loadFile(void);
+  void saveFile(void);
 
-    ///  possibly checksum
-    /// 0x8554F6F3
-    int32_t m_checksum;
-
-    /// File size in bytes
-    /// Ex:  0x003D5800
-    int32_t m_byteCount;
-
-    /// Actually used version?
-    /// 0x0B or 0x0C
-    int32_t m_version;
-
-    /// Comment
-    /// Always empty, 32 bytes
-    std::string m_comment;
-
-    std::vector<SmpFrame> m_frames;
+  //----------------------------------------------------------------------------
+  void serializeHeader(void);
 };
 
-} // namespace genie
+typedef std::shared_ptr<SmpFile> SmpFilePtr;
+
+}
+
+#endif // GENIE_SMPFILE_H
--- src/resource/SmpFrame.cpp
+++ src/resource/SmpFrame.cpp
@@ -1,118 +1,211 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2018  Mikko "Tapsa" P
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "genie/resource/SmpFrame.h"
-namespace genie {
 
-Logger &SmpFrame::log = Logger::getLogger("genie.SmpFrame");
-SmpFrame SmpFrame::null;
+#include <iostream>
+//Debug
+#include <cassert>
+#include <stdexcept>
+#include <chrono>
 
-void SmpFrame::serializeObject()
+#include "genie/resource/Color.h"
+
+namespace genie
 {
-    assert(false && "not implemented");
 
-    serialize<uint32_t>(width_);
-    serialize<uint32_t>(height_);
+Logger& SmpFrame::log = Logger::getLogger("genie.SmpFrame");
+extern const char* CNT_SETS;
 
-    serialize<int32_t>(hotspot_x);
-    serialize<int32_t>(hotspot_y);
-
-    serialize(m_frameType); // is this maybe palette offset, and heinezen misunderstood? Can't check until it is released so I can buy it
-
-    serialize(outline_table_offset_);
-    serialize(cmd_table_offset_);
-
-    // Just assume this is properties for now
-    // 0x01, 0x02 or 0x80 are example values from heinezen
-    serialize(properties_);
-
-    //----------------------------------------------------------------------------
-    /// Reads the edges of the frame. An edge int is the number of pixels in
-    /// a row which are transparent. There are two 16 bit unsigned integers for
-    /// each side of a row. One starting from left and the other starting from the
-    /// right side.
-    getIStream()->seekg(slp_file_pos_ + std::streampos(outline_table_offset_));
-
-    left_edges_.resize(height_, 0);
-    right_edges_.resize(height_, 0);
-
-    for (uint32_t row = 0; row < height_; ++row) {
-        serialize<uint16_t>(left_edges_[row]);
-        serialize<uint16_t>(right_edges_[row]);
-    }
+//------------------------------------------------------------------------------
+SmpFrame::SmpFrame()
+{
 }
 
-void SmpFrame::readImage()
+//------------------------------------------------------------------------------
+SmpFrame::~SmpFrame()
 {
-    std::istream &istr = *getIStream();
-
-    const size_t byteCount = width_ * height_;
-    smp_pixels.resize(byteCount);
-
-    size_t pixelsRead = 0;
-
-    // Each row has it's commands, 0x0F signals the end of a rows commands.
-    for (uint32_t row = 0; row < height_; ++row) {
-//        istr.seekg(slp_file_pos_ + std::streampos(cmd_offsets_[row]));
-//        std::cout << "command offset: " << cmd_offsets_[row] << std::endl;
-//        std::cout << "file pos: " << slp_file_pos_ << std::endl;
-        assert(!istr.eof());
-
-        // Transparent rows apparently read one byte anyway. NO THEY DO NOT! Ignore and use seekg()
-        if (0x8000 == left_edges_[row] || 0x8000 == right_edges_[row]) { // Remember signedness!
-            continue; // Pretend it does not exist.
-        }
-
-        uint32_t pix_pos = left_edges_[row]; //pos where to start putting pixels
-
-        while (true) {
-            const uint8_t data = read<uint8_t>();
-            if (data == 3) { // end of row
-                break;
-            }
-
-            const uint32_t pix_cnt = (data >> 2) + 1;
-
-            switch(data & 0b11) {
-            case 0: // Skip
-                break;
-            case 1: // Normal colors
-                readSmpPixelstoImage(row, pix_pos, pix_cnt, false);
-                break;
-            case 2: // Player colors
-                readSmpPixelstoImage(row, pix_pos, pix_cnt, true);
-                break;
-            }
-
-            pix_pos += pix_cnt;
-            pixelsRead += pix_cnt;
-
-        }
-    }
-
-    if (pixelsRead == 0) {
-        width_ = 0;
-        height_ = 0;
-    }
-
 }
 
-void SmpFrame::readSmpPixelstoImage(uint32_t row, uint32_t &col, uint32_t count, bool player_col)
+uint32_t SmpFrame::getWidth(void) const
 {
-    if (!player_col) {
-        getIStream()->read((char *)&smp_pixels.data()[row * width_ + col], count * sizeof(SmpPixel));
-        col += count;
-        return;
-    }
-
-    std::vector<char> bgras(count);
-    getIStream()->read(bgras.data(), count);
-
-    uint32_t to_pos = col + count;
-    while (col < to_pos) {
-        SmpPixel pixel;
-        getIStream()->read((char *)&pixel, sizeof(SmpPixel));
-        smp_player_color_mask.push_back({ col, row, pixel });
-
-        ++col;
-    }
+  return layer_width_;
 }
 
-}//namespace genie
+uint32_t SmpFrame::getHeight(void) const
+{
+  return layer_height_;
+}
+
+void SmpFrame::serializeObject(void)
+{
+}
+
+void SmpFrame::setLoadParams(std::istream &istr, uint32_t smp_offset_)
+{
+  setIStream(istr);
+  setOperation(OP_READ);
+  // This is needed because SMP frames are not stored immediately in contiguous order.
+  smp_frame_pos_ = smp_offset_;
+  istr.seekg(smp_offset_);
+}
+
+void SmpFrame::setSaveParams(std::ostream &ostr, uint32_t &smp_offset_)
+{
+  setOStream(ostr);
+  setOperation(OP_WRITE);
+}
+
+//------------------------------------------------------------------------------
+void SmpFrame::serializeHeader(void)
+{
+  serialize<uint32_t>(width_);
+  serialize<uint32_t>(height_);
+
+  serialize<int32_t>(hotspot_x);
+  serialize<int32_t>(hotspot_y);
+
+  serialize<uint32_t>(type);
+  serialize<uint32_t>(diffuse_palette_xid);
+  serialize<uint32_t>(diffuse_palette_num);
+  serialize<uint32_t>(num_layers);
+
+// Immediately after there is layer info.
+// I guess as many times as there are layers.
+// Read in the basic image layer only for now.
+  if (num_layers > 0)
+  {
+    serialize<uint32_t>(layer_width_);
+    serialize<uint32_t>(layer_height_);
+
+    serialize<int32_t>(layer_hotspot_x);
+    serialize<int32_t>(layer_hotspot_y);
+
+    serialize<uint32_t>(layer_type_);
+    serialize<uint32_t>(layer_outline_offsets_);
+    serialize<uint32_t>(layer_data_offsets_);
+    serialize<uint32_t>(layer_flags_);
+  }
+}
+
+//------------------------------------------------------------------------------
+void SmpFrame::load(std::istream &istr)
+{
+  setIStream(istr);
+
+  if (0 == num_layers)
+  {
+     log.error("No layers");
+     return;
+  }
+
+  img_data.pixel_indexes.resize(layer_width_ * layer_height_);
+  img_data.alpha_channel.resize(layer_width_ * layer_height_, 0);
+
+  istr.seekg(smp_frame_pos_ + std::streampos(layer_outline_offsets_));
+  readEdges();
+
+  istr.seekg(smp_frame_pos_ + std::streampos(layer_data_offsets_));
+  serialize<uint32_t>(cmd_offsets_, layer_height_);
+
+  // Each row has its commands.
+  for (uint32_t row = 0; row < layer_height_; ++row)
+  {
+    if (0xFFFF == left_edges_[row] || 0 == cmd_offsets_[row])
+    {
+      continue; // Skip transparent lines.
+    }
+    uint32_t pix_pos = left_edges_[row]; //Position where to start putting pixels
+    istr.seekg(smp_frame_pos_ + std::streampos(cmd_offsets_[row]));
+    assert(!istr.eof());
+
+    uint8_t data = 0;
+    while (true)
+    {
+      data = read<uint8_t>();
+
+      uint8_t cmd = data & 0x3;
+
+      if (0x3 == cmd)
+      {
+        break;
+      }
+
+      uint8_t pix_cnt = 1 + ((data & 0xFC) >> 2);
+
+      switch (cmd)
+      {
+        case 0x0: // Transparent skip
+          pix_pos += pix_cnt;
+          break;
+
+        case 0x1: // Copy pixels
+          readPixelsToImage(row, pix_pos, pix_cnt, false);
+          break;
+
+        case 0x2: // Player color pixels
+          readPixelsToImage(row, pix_pos, pix_cnt, true);
+          break;
+
+        default:
+          log.error("Unknown command [%X]", data);
+          return;
+      }
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+void SmpFrame::readEdges(void)
+{
+  left_edges_.resize(layer_height_);
+  right_edges_.resize(layer_height_);
+
+  for (uint32_t row = 0; row < layer_height_; ++row)
+  {
+    serialize<uint16_t>(left_edges_[row]);
+    serialize<uint16_t>(right_edges_[row]);
+  }
+}
+
+//------------------------------------------------------------------------------
+void SmpFrame::readPixelsToImage(uint32_t row, uint32_t &col,
+                                 uint32_t count, bool player_col)
+{
+  uint32_t to_pos = col + count;
+  while (col < to_pos)
+  {
+    uint32_t color_index = read<uint32_t>();
+    assert(row * layer_width_ + col < img_data.pixel_indexes.size());
+    img_data.pixel_indexes[row * layer_width_ + col] = color_index;
+    img_data.alpha_channel[row * layer_width_ + col] = 255;
+    if (player_col)
+    {
+      img_data.player_color_mask.push_back({col, row, uint8_t(color_index)});
+    }
+    ++col;
+  }
+}
+
+//------------------------------------------------------------------------------
+void SmpFrame::save(std::ostream &ostr)
+{
+  setOStream(ostr);
+}
+
+}
