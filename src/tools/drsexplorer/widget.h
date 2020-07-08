2f062d9a139bd040f25ca9a794c361786940cffc
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-07-08 09:35:25 +0200

    add old tool I wrote for exploring graphics in DRS files

---
+++ src/tools/drsexplorer/widget.h
@@ -0,0 +1,34 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QListWidget>
+
+#include <genie/resource/DrsFile.h>
+#include <genie/resource/PalFile.h>
+#include <memory>
+#include <QDir>
+
+class Widget : public QListWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+    bool loadPalette(const int palette, const QString &drs);
+    bool loadDrs(const QString &path);
+
+private:
+    QPixmap getPixmap(genie::SlpFramePtr frame);
+
+    void loadSlps(genie::DrsFile *file);
+    void loadSlps(const QFileInfoList &files);
+
+    genie::DrsFile m_interfaceFile;
+    genie::DrsFile m_drsFile;
+    genie::PalFile m_palette;
+    QVector<QRgb> m_colorTable;
+};
+
+#endif // WIDGET_H
