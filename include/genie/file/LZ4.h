137f434312121cca53c852e8865d2ec3bd8f5824
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-08-05 19:58:08 +0200

    don't waste member variables

--- include/genie/file/LZ4.h
+++ include/genie/file/LZ4.h
@@ -8,17 +8,15 @@
 
 struct LZ4
 {
-    void compress(const int max_chain);
-    bool decompress();
-    void compress_optimal();
+    void compress(FILE* g_in, FILE* g_out, const int max_chain);
+    bool decompress(FILE* g_in, FILE* g_out);
+    void compress_optimal(FILE* g_in, FILE* g_out);
 
 private:
-    uint8_t g_buf[LZ4_BLOCK_SIZE+LZ4_BLOCK_SIZE+LZ4_EXCESS];
     inline void wild_copy(int d, int s, int n);
     int compress_block(const int n, const int max_chain);
     bool decompress_block(int &p, int ip, const int ip_end);
-
-    FILE* g_in;
-    FILE* g_out;
+    
+    uint8_t g_buf[LZ4_BLOCK_SIZE+LZ4_BLOCK_SIZE+LZ4_EXCESS];
 };
 
