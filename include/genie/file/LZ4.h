cbf81edbbe64687101df0803d0f0916ee6cde503
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-08-05 19:53:34 +0200

    refactor out the actual compression/decompression from the file io

--- include/genie/file/LZ4.h
+++ include/genie/file/LZ4.h
@@ -15,6 +15,8 @@ struct LZ4
 private:
     uint8_t g_buf[LZ4_BLOCK_SIZE+LZ4_BLOCK_SIZE+LZ4_EXCESS];
     inline void wild_copy(int d, int s, int n);
+    int compress_block(const int n, const int max_chain);
+    bool decompress_block(int &p, int ip, const int ip_end);
 
     FILE* g_in;
     FILE* g_out;
