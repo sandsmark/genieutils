207db2613afa7c011bd3883d6335276b6918bfb5
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-08-05 19:26:15 +0200

    clean and fix up build

--- include/genie/file/LZ4.h
+++ include/genie/file/LZ4.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdint>
+#include <cstdio>
+
 #define LZ4_EXCESS (16+(LZ4_BLOCK_SIZE/255))
 #define LZ4_BLOCK_SIZE (8<<20) // 8 MB
 
@@ -7,9 +10,11 @@ struct LZ4
 {
     void compress(const int max_chain);
     bool decompress();
+    void compress_optimal();
 
 private:
-    U8 g_buf[LZ4_BLOCK_SIZE+LZ4_BLOCK_SIZE+LZ4_EXCESS];
+    uint8_t g_buf[LZ4_BLOCK_SIZE+LZ4_BLOCK_SIZE+LZ4_EXCESS];
+    inline void wild_copy(int d, int s, int n);
 
     FILE* g_in;
     FILE* g_out;
