df951377058e8d18f29f8fc392389efecffa1b0a
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-08-29 16:30:07 +0200

    add simple tool to interact with language dlls

---
+++ src/tools/pcriotool/pcriotool.cpp
@@ -0,0 +1,69 @@
+#include "genie/lang/LangFile.h"
+#include "genie/util/Utility.h"
+#include "genie/util/Logger.h"
+
+
+#include <iostream>
+#include <filesystem>
+
+#define BLOCK_SIZE 4096 // lol what do I know
+
+void printUsage(const char *appName)
+{
+    std::cerr << "\tUsage: " << appName << " list|show gamedir [string-id]" << std::endl;
+    std::cerr << "\tDat file is used to get the filenames" << std::endl;
+}
+
+int main(int argc, char *argv[]) try
+{
+    if (argc < 3) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    const std::string command = argv[1];
+
+    std::string gamePath = argv[2];
+
+    std::vector<std::string> dlls{
+        "language.dll",
+        "language_x1.dll",
+        "language_x1_p1.dll"
+    };
+
+    int id = 0;
+    if (argc > 3) {
+        id = std::stoi(argv[3]);
+    } else {
+        std::cerr << "Listing not supported yet, dll's suck" << std::endl;
+        return 1;
+    }
+
+    for (const std::string &dll : dlls) {
+        const std::string filepath = gamePath + "/" + dll;
+        if (!std::filesystem::exists(filepath)) {
+            continue;
+        }
+        try {
+            genie::LangFile langfile;
+            langfile.load(filepath);
+            if (id >= 0) {
+                const std::string str = langfile.getString(id);
+                if (!str.empty()) {
+                    std::cout << id << '\t' << dll << "\t'" << str << "'" << std::endl;
+                }
+                continue;
+            }
+        } catch (const std::exception &error) {
+            std::cerr << error.what() << std::endl;
+        }
+    }
+
+
+
+    return 0;
+} catch (const std::exception &e) { // just in case I forget to catch something, I hate exceptions
+    std::cerr << "Exception: " << e.what() << std::endl;
+    return 1;
+}
+
