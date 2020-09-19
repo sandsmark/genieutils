1a1071dcd46296542a5bbb974fec53b278a99be2
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-09-19 08:12:56 +0200

    store game data path in qsettings in picviewer

--- src/tools/picviewer/main.cpp
+++ src/tools/picviewer/main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 #include <QCommandLineParser>
 #include <iostream>
+#include <QSettings>
 
 #include "genie/resource/SmxFile.h"
 
@@ -25,14 +26,20 @@ int main(int argc, char *argv[])
     QCommandLineOption drsPath("drs", "Path to DRS file to load from", "drs identifier");
     QCommandLineOption palFile("palette", "Palette path/name to use (50500 from interfac.drs is the default)", "identifier", "50500");
     QCommandLineOption palDrs("palettedrs", "DRS file to load palette from", "drs identifier");
+    QCommandLineOption dataPath("gamepath", "Path to game directory", "path");
 
     parser.addHelpOption();
     parser.addVersionOption();
     parser.addOption(drsPath);
     parser.addOption(palFile);
     parser.addOption(palDrs);
+    parser.addOption(dataPath);
     parser.addPositionalArgument("picturefile", QCoreApplication::translate("file", "Path/name of picture to show"));
     parser.process(a);
+    if (parser.isSet(dataPath)) {
+        QSettings settings;
+        settings.setValue("datapath", parser.value(dataPath));
+    }
 
     Window w;
     if (parser.isSet(drsPath)) {
